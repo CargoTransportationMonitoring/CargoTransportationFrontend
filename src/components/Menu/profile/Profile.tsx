c18729b39244f6a52a0a58672308cc0a2ba5be79
nikitaivanov <iwaa0303@mail.ru>
2024-12-30 21:05:08 +0300

    added profile edit/delete

---
+++ src/components/Menu/profile/Profile.tsx
@@ -0,0 +1,93 @@
+import axios from "axios";
+import React, {JSX, useState} from "react";
+import {authenticate, parseJwt, TokenId} from "../../../util/KeycloakUtils";
+import {getIdToken, getToken} from "../../auth/KeycloakService";
+import {API_V1_USER_PREFIX, SERVER_URI} from "../../../util/Constants";
+
+const Profile: React.FC = (): JSX.Element => {
+
+    const tokenData: TokenId = parseJwt(getIdToken());
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [firstName, setFirstName] = useState<string>(tokenData.given_name);
+    const [lastName, setLastName] = useState<string>(tokenData.family_name);
+
+    const toggleEditMode = (): void => {
+        if (isEditing) {
+            axios.put(
+                `${SERVER_URI}/${API_V1_USER_PREFIX}/${tokenData.sub}`,
+                {
+                    userId: tokenData.sub,
+                    name: firstName,
+                    surname: lastName,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${getToken()}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+                .then((): void => {
+                    console.log("Данные успешно сохранены:", {firstName, lastName});
+                })
+                .catch((error): void => {
+                    console.error("Ошибка при сохранении данных:", error);
+                });
+        }
+        setIsEditing(!isEditing);
+    };
+
+    const deleteProfile = (): void => {
+        axios.delete(`${SERVER_URI}/${API_V1_USER_PREFIX}/${tokenData.sub}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${getToken()}`,
+                    "Content-Type": "application/json"
+                }
+            })
+            .then((): void => {
+                console.log("Профиль успешно удален")
+                authenticate()
+            })
+            .catch((error): void => {
+                console.error("Ошибка при удалении профиля:", error);
+            });
+    }
+
+    return (
+        <div>
+            <h2>Email: {tokenData.email}</h2>
+            <div>
+                <label>First name:</label>
+                {isEditing ? (
+                    <input
+                        type="text"
+                        value={firstName}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)}
+                    />
+                ) : (
+                    <h2>{firstName}</h2>
+                )}
+            </div>
+            <div>
+                <label>Last name:</label>
+                {isEditing ? (
+                    <input
+                        type="text"
+                        value={lastName}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)}
+                    />
+                ) : (
+                    <h2>{lastName}</h2>
+                )}
+            </div>
+            <button onClick={toggleEditMode}>
+                {isEditing ? "Сохранить" : "Редактировать"}
+            </button>
+
+            <button onClick={deleteProfile}>Удалить профиль</button>
+        </div>
+    );
+}
+
+export default Profile;
--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -1,6 +1,6 @@
-export const KEYCLOAK_URI: string = 'http://localhost:8080/realms/cargotransportation-realm/protocol/openid-connect';
 export const KEYCLOAK_URL: string = "http://localhost:8080"
 export const KEYCLOAK_REALM: string = "cargotransportation-realm"
+export const KEYCLOAK_URI: string = `${KEYCLOAK_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect`;
 export const KEYCLOAK_CLIENT_ID: string = "cargotransportation-client"
 export const KEYCLOAK_RESPONSE_TYPE: string = 'code';
 export const KEYCLOAK_SCOPE: string = 'openid';
@@ -9,5 +9,8 @@ export const KEYCLOAK_CODE_CHALLENGE_METHOD: string = 'S256';
 export const CLIENT_ROOT_URI: string = 'http://localhost:3000/main'
 
 
-export const RESOURCE_SERVER_ADMIN_URI: string = 'http://localhost:8081/test/admin'
-export const RESOURCE_SERVER_USER_URI: string = 'http://localhost:8081/test/user'
+export const SERVER_ADMIN_URI: string = 'http://localhost:8081/test/admin'
+export const SERVER_USER_URI: string = 'http://localhost:8081/test/user'
+export const SERVER_URI: string = 'http://localhost:8081'
+
+export const API_V1_USER_PREFIX: string = 'api/v1/user'
