3d39e13512c8b07433f7eb6087f6620fa145c13b
nikitaivanov <iwaa0303@mail.ru>
2025-02-09 18:53:14 +0300

    fixed style places

--- src/components/Administrator/Tabs/StatusTabs.tsx
+++ src/components/Administrator/Tabs/StatusTabs.tsx
@@ -1,6 +1,7 @@
 import React, {FC, JSX, useState} from "react";
 import {setStatus} from "../../../redux/slices/FilterSlice";
 import {Dispatch} from "@reduxjs/toolkit";
+import styles from "./StatusTabs.module.css"
 import {useDispatch} from "react-redux";
 
 const TABS: string[] = ["NEW", "IN_PROGRESS", "COMPLETED"];
@@ -17,12 +18,14 @@ const StatusTabs: FC = (): JSX.Element => {
     };
 
     return (
-        <div className="tabs">
+        <div className={styles.tabs}>
             {TABS.map((tab: string) => (
                 <button
                     key={tab}
-                    className={`tab ${activeTab === tab ? "active" : ""}`}
+                    className={`${styles.tab} ${activeTab === tab ? styles.active : ""}`}
                     onClick={() => handleTabChange(tab)}
+                    tabIndex={0}
+                    aria-selected={activeTab === tab}
                 >
                     {tab.replace("_", " ").toUpperCase()}
                 </button>
